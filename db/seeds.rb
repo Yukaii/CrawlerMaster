ca81b0e1dca96cb792b4d2847a89322e1e888abf
Yukai Huang <yukaihuangtw@gmail.com>
2016-07-26 15:26:55 +0800

    update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.create(email: 'admin@dev.null', username: 'admin', password: 'password', password_confirmation: 'password')
+
+def current_year
+  (Time.zone.now.month.between?(1, 7) ? Time.zone.now.year - 1 : Time.zone.now.year)
+end
+
+def current_term
+  (Time.zone.now.month.between?(2, 7) ? 2 : 1)
+end
+
+CourseCrawler.crawler_list.map(&:to_s).each do |crawler_name|
+  crawler = Crawler.find_or_create_by(
+    name: crawler_name
+  )
+
+  crawler.update!(
+    schedule: { in: '1s' },
+    save_to_db: true,
+    year: current_year,
+    term: current_term
+  )
+end
