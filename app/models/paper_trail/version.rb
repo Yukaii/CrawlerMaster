58c1dd6ad84d054f70e1edc6b0da5e630261a6f5
Yukai Huang <yukai@colorgy.io>
2016-08-08 18:21:42 +0800

    fix paper_trail loading

--- app/models/paper_trail/version.rb
+++ app/models/paper_trail/version.rb
@@ -1,16 +1,20 @@
-class PaperTrail::Version < ActiveRecord::Base
-  has_many :course_task_relations, foreign_key: :version_id
-  has_many :course_tasks, through: :course_task_relations, source: :crawl_task
+module PaperTrail
+  class Version < ActiveRecord::Base
+    include PaperTrail::VersionConcern
 
-  # a PaperTrail::Version would only have one corresponding course_task
-  # we build many-to-many relationship in order not to ruin the PaperTrail database schema
-  def course_task
-    course_tasks.first
+    has_many :course_task_relations, foreign_key: :version_id
+    has_many :course_tasks, through: :course_task_relations, source: :crawl_task
+
+    # a PaperTrail::Version would only have one corresponding course_task
+    # we build many-to-many relationship in order not to ruin the PaperTrail database schema
+    def course_task
+      course_tasks.first
+    end
+
+    # delgate course_errors to relation object
+    def course_errors
+      course_task_relations.first.course_errors
+    end
+
   end
-
-  # delgate course_errors to relation object
-  def course_errors
-    course_task_relations.first.course_errors
-  end
-
 end
