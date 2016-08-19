81e4a312fc874b7c79b16252f7277517f6bb754e
Yukai Huang <yukai@colorgy.io>
2016-08-19 16:08:08 +0800

    fix csu period

--- lib/course_crawler/crawlers/csu_course_crawler.rb
+++ lib/course_crawler/crawlers/csu_course_crawler.rb
@@ -16,22 +16,7 @@ class CsuCourseCrawler < CourseCrawler::Base
     "U" => 7
     }
 
-  PERIODS = {
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9,
-    "A" => 10,
-    "B" => 11,
-    "C" => 12,
-    "D" => 13,
-    "E" => 14
-    }
+  PERIODS = CoursePeriod.find('CSU').code_map
 
   def initialize year: nil, term: nil, update_progress: nil, after_each: nil
 
