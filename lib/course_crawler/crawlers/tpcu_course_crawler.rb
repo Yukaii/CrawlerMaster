de4d4f33299be682df88fedbd1f92cdd8a7c042c
herber523 <herber523@gmail.com>
2016-08-28 01:25:12 +0800

    fix ntub ntunhs tpcu periods

--- lib/course_crawler/crawlers/tpcu_course_crawler.rb
+++ lib/course_crawler/crawlers/tpcu_course_crawler.rb
@@ -16,25 +16,7 @@ class TpcuCourseCrawler < CourseCrawler::Base
     "六" => 6,
     "日" => 7
     }
-
-  PERIODS = {
-    "1" => 1,
-    "2" => 2,
-    "3" => 3,
-    "4" => 4,
-    "5" => 5,
-    "6" => 6,
-    "7" => 7,
-    "8" => 8,
-    "9" => 9,
-    "10" => 10,
-    "K" => 10,
-    "A" => 11,
-    "B" => 12,
-    "C" => 13,
-    "D" => 14,
-    "E" => 15
-    }
+	PERIODS = CoursePeriod.find('TPCU').code_map
 
   def initialize year: nil, term: nil, update_progress: nil, after_each: nil
 
