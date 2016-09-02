3b1476ccaba777540b52e0c527826441b757dbf9
Yukai Huang <yukai@colorgy.io>
2016-09-02 18:03:29 +0800

    fix nfu period data

--- lib/course_crawler/crawlers/nfu_course_crawler.rb
+++ lib/course_crawler/crawlers/nfu_course_crawler.rb
@@ -6,67 +6,82 @@
 module CourseCrawler::Crawlers
 class NfuCourseCrawler < CourseCrawler::Base
 
-	def initialize year: nil, term: nil, update_progress: nil, after_each: nil
+  PERIODS = Hash[CoursePeriod.find('NFU').periods.select { |p| 1 <= p.order && p.order <= 14 }.map { |p| [p.code, p.order] }]
+  PERIODS_WEEKEND = Hash[CoursePeriod.find('NFU').periods.select { |p| 15 <= p.order && p.order <= 30 }.map { |p| [p.code, p.order] }]
+
+  def initialize year: nil, term: nil, update_progress: nil, after_each: nil
     @year                 = year || current_year
     @term                 = term || current_term
     @post_url             = "http://osa.nfu.edu.tw/query/class_ajax.php"
     @post_url_class       = "http://osa.nfu.edu.tw/query/classlist.php"
     @update_progress_proc = update_progress
     @after_each_proc      = after_each
-	end
+  end
 
-	def courses
-		@courses = []
-		puts "get url ..."
-		dep = []
-		year = (@year-1911).to_s
-		term = @term.to_s
-		all_yt = year + term
+  def courses
+    @courses = []
+    puts "get url ..."
+    dep = []
+    year = (@year-1911).to_s
+    term = @term.to_s
+    all_yt = year + term
 
-		# search dep
-		r = RestClient.post( @post_url_class , { pselclss: '12011' })
-		doc = Nokogiri::HTML(r)
+    # search dep
+    r = RestClient.post( @post_url_class , { pselclss: '12011' })
+    doc = Nokogiri::HTML(r)
 
-		doc.css('select[id="selclss"] option')[1..-1].each do |department|
-			dep << department.text[0..4]
-		end
+    doc.css('select[id="selclss"] option')[1..-1].each do |department|
+      dep << department.text[0..4]
+    end
 
-		#do search datas
-		dep.each do |dep_no|
-			puts"data crawled : "+"#{dep.index(dep_no)+1} / #{dep.size.to_s} -> " + dep_no
+    dept_names = doc.css('select[id="selclss"] option')[1..-1].map(&:text)
 
-			r = RestClient.post( @post_url , {
-					pselyr: all_yt,
-					pselclss: dep_no,
-				})
+    #do search datas
+    dep.each_with_index do |dep_no, dep_index|
+      puts"data crawled : "+"#{dep.index(dep_no)+1} / #{dep.size.to_s} -> " + dep_no
 
-			doc = Nokogiri::HTML(r)
+      r = RestClient.post( @post_url , {
+          pselyr: all_yt,
+          pselclss: dep_no,
+        })
 
-			talbe_un = doc.css('div[id="copyall"]')
-			if (talbe_un.text.size > 0)
-				index = doc.css('div[id="copyall"]').css('table tr')
-				index[3..-1].each do |row|
-					datas = row.css('td')
+      doc = Nokogiri::HTML(r)
 
-					course_days = []
-					course_periods = []
-					course_locations = []
+      talbe_un = doc.css('div[id="copyall"]')
+      if (talbe_un.text.size > 0)
+        index = doc.css('div[id="copyall"]').css('table tr')
+        index[3..-1].each do |row|
+          datas = row.css('td')
 
-					datas[6..12].each do |day_class|
-						if(day_class.text != "&nbsp")
-							day_have_class = (datas.index(day_class).to_i - 5)
-							day_have_periods = day_class.text.split(',')
-							day_have_periods.each do |_periods|
-								course_days << day_have_class
-								course_periods << _periods
-								course_locations << datas[13].text.strip
-							end
-						end
-					end
+          course_days = []
+          course_periods = []
+          course_locations = []
 
-					next if datas[1].text.strip.empty?
+          datas[6..12].each do |day_class|
+            next if day_class.text == "&nbsp" || day_class.text.delete("\u00a0").empty?
 
-					course = {
+            day_have_class = (datas.index(day_class).to_i - 5)
+            day_have_periods = day_class.text.split(',')
+            day_have_periods.each do |period|
+              dept_n = dept_names[dep_index]
+              course_period = if dept_n.include?("在職專班") || dept_n.include?("進") || dept_n.include?("夜") || dept_n.include?("二技") ||
+                                 dept_n.include?("動機系")
+                                PERIODS_WEEKEND[period]
+                              else
+                                PERIODS[period]
+                              end
+
+              next if course_period.nil? # 不考慮不正確的節次，呵
+
+              course_days << day_have_class
+              course_periods << course_period
+              course_locations << datas[13].text.strip
+            end
+          end
+
+          next if datas[1].text.strip.empty?
+
+          course = {
             name:         "#{datas[1].text.strip}",
             year:         @year,
             term:         @term,
@@ -102,15 +117,15 @@ class NfuCourseCrawler < CourseCrawler::Base
             location_7:   course_locations[6],
             location_8:   course_locations[7],
             location_9:   course_locations[8],
-					}
+          }
 
-					@after_each_proc.call(course: course) if @after_each_proc
-					@courses << course
-				end
-			end
-		end
-		puts "Project finished !!!"
-		@courses
-	end
+          @after_each_proc.call(course: course) if @after_each_proc
+          @courses << course
+        end
+      end
+    end
+    puts "Project finished !!!"
+    @courses
+  end
 end
 end
